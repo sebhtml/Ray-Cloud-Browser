ef8ca71e0fd8841eb7d901b13a7a21585bfd842b
Sébastien Boisvert <sebastien.boisvert.3@ulaval.ca>
2012-12-03 14:17:00 -0500

    The datastore can overwrite the depth
    Signed-off-by: Sébastien Boisvert <sebastien.boisvert.3@ulaval.ca>

--- client/DataStore.js
+++ client/DataStore.js
@@ -191,9 +191,11 @@ DataStore.prototype.getKmerInformation=function(kmerSequence,graphOperator){
 			}
 		}
 
+		var depth=512;
 		var address="/cgi-bin/RayCloudBrowser.webServer.cgi?";
 		address+="tag=RAY_MESSAGE_TAG_GET_KMER_FROM_STORE";
 		address+="&content="+kmerSequence;
+		address+="&depth="+depth;
 		xmlHttp.open("GET",address,true);
 		xmlHttp.send(null);
 		this.httpRequests++;
