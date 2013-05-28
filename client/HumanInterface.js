649bc6d7fd843d68e5069649765c9a43f9308e63
Jean-François Erdelyi <jean-francois.erdelyi.1@ulaval.ca>
2013-05-28 16:47:10 -0400

    Client: fix Double click are not all consumed
    Link: https://github.com/sebhtml/Ray-Cloud-Browser/issues/34
    Signed-off-by: Jean-François Erdelyi <jean-francois.erdelyi.1@ulaval.ca>

--- client/HumanInterface.js
+++ client/HumanInterface.js
@@ -408,8 +408,8 @@ HumanInterface.prototype.draw=function() {
 }
 
 HumanInterface.prototype.handleMouseDoubleClick=function(x,y) {
-	this.doubleClick = true;
 	if(this.handleMouseDown(x, y)) {
+		this.getInventory().getSelector().handleMouseDoubleClick(x, y);
 		return true;
 	}
 
@@ -422,11 +422,6 @@ HumanInterface.prototype.handleMouseDoubleClick=function(x,y) {
 HumanInterface.prototype.handleMouseDown=function(x,y){
 
 	if(this.sampleInventory.handleMouseDown(x,y)) {
-		if(this.doubleClick && this.getInventory().getSelector().handleMouseDoubleClick(x, y)) {
-			this.doubleClick = false;
-			return true;
-		}
-
 		if(this.getInventory().getSelector().hasChoices()) {
 			this.locationData=this.getInventory().getSelector().getLocationData();
 		}
